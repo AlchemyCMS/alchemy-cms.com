6b90eba7c6dec96b7eb0c083a3f3a7fe2a71ce4b
Thomas von Deyen <tvdeyen@gmail.com>
2012-10-23 23:20:19 +0200

    Updated Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,108 +1,74 @@
-require "bundler/capistrano"
-require "alchemy/capistrano"
+require 'bundler/capistrano'
+require 'alchemy/capistrano'
 load 'deploy/assets'
 
-set :application, "alchemy-cms"
-set :repository,  "git@magiclabs.de:alchemy-app.git"
-set :branch, 'master'
+# ssh settings
+set :user,                      "rails3"
+set :ssh_options,               { :forward_agent => true }
+ssh_options[:keys] =            [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+default_run_options[:pty] =     true
 
-set :scm, :git
-set :deploy_via, :remote_cache
-set :copy_exclude, [".svn", ".DS_Store"]
+# domain names
+role :app,                      "server1904.railsvserver.de"
+role :web,                      "server1904.railsvserver.de"
+role :db,                       "server1904.railsvserver.de", :primary => true
 
-set :use_sudo, false
-set :port, 12312
+# the webserver path
+set :deploy_to,                 "/var/www/alchemy-homepage"
 
-set :user, "web28"
-set :password, "hym1guv3"
+# repository settings
+set :scm,                       "git"
+set :repository,                "ssh://git@magiclabs.de:12312/alchemy-app.git"
+set :branch,                    "master"
 
-role :web, "rv2.nethosting4you-server.de"                          # Your HTTP server, Apache/etc
-role :app, "rv2.nethosting4you-server.de"                          # This may be the same as your `Web` server
-role :db,  "rv2.nethosting4you-server.de", :primary => true        # This is where Rails migrations will run
+# before hooks
+before "deploy",                "deploy:web:disable"
+before "deploy:start",          "deploy:seed"
+before "deploy:create_symlink", "deploy:migrate"
 
-set :deploy_to, "/var/www/#{user}/html/#{application}_gfx"
+# after hooks
+after "deploy:setup",           "alchemy:database_yml:create"
+after "deploy:assets:symlink",  "alchemy:database_yml:symlink"
+after "deploy",                 "deploy:cleanup"
+after "deploy",                 "deploy:web:enable"
 
-after "deploy:setup", "deploy:db:setup" unless fetch(:skip_db_setup, false)
-before "deploy:start", "deploy:seed"
-after "deploy:symlink", "deploy:db:symlink"
-before "deploy:restart", "deploy:migrate"
-after "deploy", "deploy:cleanup"
+# special tasks
 
 namespace :logs do
-  desc "show last 100 lines of production.log"
+  desc "show last 100 lines of log"
   task :tail do
-    run "tail -n100 #{shared_path}/log/production.log"
+    run "tail -n100 #{shared_path}/log/#{rails_env}.log"
   end
 
-  desc "watch tail of production.log and wait for additional data to be appended to the input"
+  desc "watch tail of log and wait for additional data to be appended to the input"
   task :watch do
-    stream("tail -f #{shared_path}/log/production.log")
+    stream("tail -f #{shared_path}/log/#{rails_env}.log")
   end
 end
 
 namespace :deploy do
 
-	task :start do ; end
-	task :stop do ; end
+  task :start do ; end
+  task :stop do ; end
 
-	task :restart, :roles => :app, :except => { :no_release => true } do
-	  run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-	end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
 
-	desc 'Seeds the database'
-	task :seed, :roles => :app, :except => { :no_release => true } do
-		run "cd #{release_path} && RAILS_ENV=production #{rake} db:seed"
-	end
-
-	namespace :db do
-
-		desc <<-DESC
-      Creates the database.yml configuration file in shared path.
-
-      By default, this task uses a template unless a template \
-      called database.yml.erb is found either is :template_dir \
-      or /config/deploy folders. The default template matches \
-      the template for config/database.yml file shipped with Rails.
-
-      When this recipe is loaded, db:setup is automatically configured \
-      to be invoked after deploy:setup. You can skip this task setting \
-      the variable :skip_db_setup to true. This is especially useful \ 
-      if you are using this recipe in combination with \
-      capistrano-ext/multistaging to avoid multiple db:setup calls \ 
-      when running deploy:setup for all stages one by one.
-    DESC
-
-		task :setup, :except => { :no_release => true } do
-
-			default_template = <<-EOF
-      production:
-        adapter: mysql2
-        encoding: utf8
-        reconnect: false
-        pool: 5
-        database: usr_web28_4
-        username: web28
-        password: pBCcLenz
-        socket: /var/run/mysqld/mysqld.sock
-      EOF
-
-			location = fetch(:template_dir, "config/deploy") + '/database.yml.erb'
-			template = File.file?(location) ? File.read(location) : default_template
-
-			config = ERB.new(template)
-
-			run "mkdir -p #{shared_path}/config" 
-			put config.result(binding), "#{shared_path}/config/database.yml"
-		end
-
-		desc <<-DESC
-			[internal] Updates the symlink for database.yml file to the just deployed release.
-		DESC
-		task :symlink, :except => { :no_release => true } do
-			run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
-		end
-
+  desc 'Seeds the database'
+  task :seed, :roles => :app, :except => { :no_release => true } do
+    run "cd #{release_path} && RAILS_ENV=#{rails_env} #{rake} db:seed"
   end
 
 end
 
+## defaults
+
+# rails env
+set :rails_env,                 "production"
+
+# enable bundler binstubs
+set :bundle_flags,              "--deployment --binstubs"
+
+# do not use sudo
+set :use_sudo,                  false
