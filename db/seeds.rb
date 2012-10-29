504f3f25d103881021c1b880c806698a087d56bb
Thomas von Deyen <tvdeyen@gmail.com>
2012-10-29 20:53:54 +0100

    Using EssencePictures and picture gallery the correct way.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,17 @@
-# Seeding Alchemy data
-Alchemy::Seeder.seed!
+Alchemy::Element.named(["slide"]).each do |element|
+  element.contents.essence_pictures.each do |content|
+    content.update_column(:name, "image")
+  end
+end
+
+Alchemy::Element.named(["screenshot"]).each do |element|
+  element.contents.essence_pictures.each do |content|
+    content.update_column(:name, "screen")
+  end
+end
+
+Alchemy::Element.named(["gallery"]).each do |element|
+  element.contents.essence_pictures.each_with_index do |content, i|
+    content.update_column(:name, "essence_picture_#{i+1}")
+  end
+end
