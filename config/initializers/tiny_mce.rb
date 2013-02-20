830e93a4845c60c80ee412a3de286fba9cf4fdc9
Thomas von Deyen <tvdeyen@gmail.com>
2013-02-20 10:39:30 +0100

    Footer styling

--- config/initializers/tiny_mce.rb
+++ config/initializers/tiny_mce.rb
@@ -1,6 +1,5 @@
 Alchemy::Tinymce.init = {
-	:theme_advanced_buttons2 => 'pastetext,pasteword,styleselect,charmap,code,help',
-	:style_formats => [
-		{:title => 'Code', :inline => 'code'}
-	]
+	:theme_advanced_buttons2 => 'pastetext,pasteword,formatselect,styleselect,charmap,code,help',
+	:style_formats => [{:title => 'Code', :inline => 'code'}],
+  :theme_advanced_blockformats => "h3,p"
 }
