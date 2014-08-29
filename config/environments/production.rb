5783ae29c7b0ef9fb2f10686d854994f8c7a2e38
Thomas von Deyen <tvd@magiclabs.de>
2014-08-29 22:30:54 +0200

    Adds smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ AlchemyCmsCom::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "support@magiclabs.de",
+    password: "he4Gsswt",
+    address: "mail.magicsupport.de",
+    port: 587,
+    domain: "magicsupport.de",
+    enable_starttls_auto: true,
+    authentication: :login
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
