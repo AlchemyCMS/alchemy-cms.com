740d18e5f31155d95dd275c15a42cb8adba26dec
Thomas von Deyen <tvd@magiclabs.de>
2014-08-29 20:44:27 +0200

    Limit the keywords list to 5

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -13,7 +13,7 @@ class ExtensionsController < ApplicationController
     end
     @extensions = @extensions.page(params[:page] || 1).per(5)
 
-    @keywords = Extension.tag_counts_on(:keywords).order('count DESC')
+    @keywords = Extension.tag_counts_on(:keywords).order('count DESC, RAND()').limit(5)
   end
 
   def show
