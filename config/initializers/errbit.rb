2cecb25ea1a95caa42725ecc5bfa3cff57c07e0b
Thomas von Deyen <tvd@magiclabs.de>
2014-09-03 13:09:39 +0200

    Remove disabling of SSL verifying
    We have a wildcard certificate now \o/

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,4 @@
 Airbrake.configure do |config|
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   config.api_key = '92c81da55ed0898d59bbc4f707ea9a86'
   config.host    = 'errors.magiclabs.de'
   config.port    = 443
