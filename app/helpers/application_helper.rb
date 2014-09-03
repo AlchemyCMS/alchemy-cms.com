5e9988ffebbb8f606b1d0074fff93e939cbbdba1
Thomas von Deyen <tvd@magiclabs.de>
2014-09-03 14:39:08 +0200

    Adds a global page for managing showcases content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def footer_page
     @footer_page ||= Alchemy::Page.find_by(page_layout: 'footer', language_id: session[:alchemy_language_id])
   end
+
+  def showcases_page
+    @showcases_page ||= Alchemy::Page.find_by(page_layout: 'showcases', language_id: session[:alchemy_language_id])
+  end
 end
