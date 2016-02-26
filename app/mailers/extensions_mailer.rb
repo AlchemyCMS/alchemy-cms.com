a9875a162e10c740842835deb0643bf51a458800
Thomas von Deyen <tvd@bitspire.de>
2016-02-26 13:55:17 +0100

    Send as alchemy@magiclabs.de

--- app/mailers/extensions_mailer.rb
+++ app/mailers/extensions_mailer.rb
@@ -1,5 +1,5 @@
 class ExtensionsMailer < ActionMailer::Base
-  default from: "hello@alchemy-cms.com"
+  default from: "alchemy@magiclabs.de"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
