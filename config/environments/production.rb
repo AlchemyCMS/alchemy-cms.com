3bd6439300b9c2ce23c7eeaf2d3ea61558df12c1
Thomas von Deyen <tvd@bitspire.de>
2016-03-11 17:52:10 +0100

    Do not send HELO to mail relay

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ AlchemyCmsCom::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "magicsupport.de.relay.cloud-security.net",
-    domain: "magiclabs.de",
     enable_starttls_auto: false,
     port: 25
   }
