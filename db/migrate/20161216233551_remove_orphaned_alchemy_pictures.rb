18fefdcc14ae98047f9714d84a4b6ec237284a35
Thomas von Deyen <tvd@bitspire.de>
2016-12-17 00:45:38 +0100

    Fix delete orphaned pictures sql syntax

--- db/migrate/20161216233551_remove_orphaned_alchemy_pictures.rb
+++ db/migrate/20161216233551_remove_orphaned_alchemy_pictures.rb
@@ -1,7 +1,7 @@
 class RemoveOrphanedAlchemyPictures < ActiveRecord::Migration
   def up
     execute <<-SQL.strip_heredoc
-      DELETE * FROM alchemy_pictures WHERE image_file_format IS NULL
+      DELETE FROM alchemy_pictures WHERE image_file_format IS NULL
     SQL
   end
 end
