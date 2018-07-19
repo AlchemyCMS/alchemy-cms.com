215736c0bd774b9599f92e960b86df370cc77b84
Thomas von Deyen <thomas@vondeyen.com>
2018-07-19 18:53:55 +0200

    Fix smtp port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: 465,
+    port: 587,
     user_name: 'apikey',
     password: ENV['SENDGRID_API_KEY'],
     enable_starttls_auto: true
